42c4e98eb76b11ca3c82722dbbf3c870bf959d3d
Андрей <andrey0sintsev@mail.ru>
2023-04-25 19:39:49 +0500

    Lab3 done

--- src/com/company/lab_3/Vector3DTest.java
+++ src/com/company/lab_3/Vector3DTest.java
@@ -1,53 +1,40 @@
 package com.company.lab_3;
 
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Test.*;
+import java.util.Scanner;
 
 class Vector3DTest
 {
-    Vector3D v = new Vector3D(3,4,5);
-
-    @Test
-    void scalar()
+    public static void main(String[] args)
     {
-        assertEquals(v.scalar(new Vector3D(2, 2, 2)), 24);
+        Scanner sc = new Scanner(System.in);
+
+        String[] strings = sc.nextLine().split(" ");
+        Vector3D a = new Vector3D(
+                Integer.parseInt(strings[0]),
+                Integer.parseInt(strings[1]),
+                Integer.parseInt(strings[2])
+        );
+
+        strings = sc.nextLine().split(" ");
+        Vector3D b = new Vector3D(
+                Integer.parseInt(strings[0]),
+                Integer.parseInt(strings[1]),
+                Integer.parseInt(strings[2])
+        );
+
+        strings = sc.nextLine().split(" ");
+        Vector3D c = new Vector3D(
+                Integer.parseInt(strings[0]),
+                Integer.parseInt(strings[1]),
+                Integer.parseInt(strings[2])
+        );
+
+        Segment segment = new Segment(a, b);
+
+        System.out.println(segment.len());
+        System.out.println(segment.distanceTo(c));
+
     }
 
-    @Test
-    void len()
-    {
-        assertEquals(v.len(), Math.sqrt(50));
-    }
-
-    @Test
-    void multiply()
-    {
-        assertEquals(v.multiply(2), new Vector3D(6,8,10));
-    }
-
-    @Test
-    void add()
-    {
-        assertEquals(v.add(new Vector3D(1,1,1)), new Vector3D(4,5,6));
-    }
-
-    @Test
-    void sub()
-    {
-        assertEquals(v.sub(new Vector3D(1,1,1)), new Vector3D(2,3,4));
-    }
-
-    @Test
-    void vecX()
-    {
-        assertEquals(v.vecX(new Vector3D(3,4,5)), new Vector3D(0,0,0));
-    }
-
-    @Test
-    void pCollin()
-    {
-        assertTrue(v.pCollin(new Vector3D(-3, -4, -5)));
-    }
 }
