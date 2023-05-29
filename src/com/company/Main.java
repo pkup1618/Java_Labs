a09b8d33c6cdd8e4fdb2006b94267350f4fea62b
Андрей <andrey0sintsev@mail.ru>
2023-05-29 09:53:30 +0500

    Lab7

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,10 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+	Triangle triangle = new Triangle("tr", 0, 0, 0, 7, 0, 6 );
+    System.out.println(triangle.getMedianLength());
+    triangle.getType();
     }
 }
